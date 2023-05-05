a31a4737df6df023bf79a51f12eabc3daaaf91f5
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-05-05 10:51:06 +0530

    #include<stdio.h> int main() {     int n,r,s=0,temp;     scanf("%d",&n);     temp=n;     while(n!=0)     {         r=n%10;         s=s*10+r;         n=n/10;     }     if(temp==s)     {         printf("True");     }     else     {         printf("False");     } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,t,s=0;
+    int n,r,s=0,temp;
     scanf("%d",&n);
-    t=n;
-    while(n>0)
+    temp=n;
+    while(n!=0)
     {
         r=n%10;
-        n=n/10;
         s=s*10+r;
+        n=n/10;
     }
-    if(t==s)
+    if(temp==s)
     {
         printf("True");
     }
