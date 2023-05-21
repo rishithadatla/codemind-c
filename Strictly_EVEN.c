037847743e47d4034ff5da8c50645003c619c778
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-05-21 12:34:50 +0530

    #include<stdio.h> int main() {     int n,i,j,a[100],s,c,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         int s=0;         if(i%2==0)         {             s++;             int c=0;             if(a[i]%2==0)             {                 c++;             }         }         else         {             k=0;             if(a[i]%2==0)             {                 k++;             }         }     }     if(s==c && k==0)     {         printf("True");     }     else if(k>0)     {         printf("False");     } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,a[100],s,c,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        int s=0;
+        if(i%2==0)
+        {
+            s++;
+            int c=0;
+            if(a[i]%2==0)
+            {
+                c++;
+            }
+        }
+        else
+        {
+            k=0;
+            if(a[i]%2==0)
+            {
+                k++;
+            }
+        }
+    }
+    if(s==c && k==0)
+    {
+        printf("True");
+    }
+    else if(k>0)
+    {
+        printf("False");
+    }
+}
