8324c6d72b1671a32dd8670f7f971006122be15d
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-05-22 21:54:58 +0530

    #include<stdio.h> int main() {     int tt,a,b,c,i;     scanf("%d",&tt);     for(i=1;i<=tt;i++)     {         scanf("%d%d",&a,&b);         c=a+b;         printf("%d ",c);         c=0;     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int tt,a,b,c,i;
+    scanf("%d",&tt);
+    for(i=1;i<=tt;i++)
+    {
+        scanf("%d%d",&a,&b);
+        c=a+b;
+        printf("%d
+",c);
+        c=0;
+    }
+}
